81b693acc98a8248da803ddb2bd6efe55f5522fb
Ricardo.SPTECH <ricardo.inoue@sptech.school>
2026-06-05 22:34:37 -0300

    Dashboard Backend

---
+++ Tomates/entrega-oficial/Site/src/routes/dashboard.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var router = express.Router();
+
+
+var dashboardController = require("../controllers/dashboardController");
+
+router.post("/cadastrar", function (req, res) {
+   
+    dashboardController.cadastrar(req, res);
+});
+
+router.get("/listar", function (req, res) {
+
+    dashboardController.listar(req, res);
+});
+
+module.exports = router;
