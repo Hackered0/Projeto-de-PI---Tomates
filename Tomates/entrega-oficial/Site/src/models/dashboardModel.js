578eb713a51027ecd64928acd580ec40bd3af682
Hackered0 <eduarda.crispim@sptech.school>
2026-06-06 21:08:50 -0300

    Tirando comentarios da  model da dashboard

--- Tomates/entrega-oficial/Site/src/models/dashboardModel.js
+++ Tomates/entrega-oficial/Site/src/models/dashboardModel.js
@@ -67,10 +67,3 @@ module.exports = {
     buscarAlertasHoje
 };
 
-// Fiz uma gambiarra pq n lembro como usa o current(), então se liga
-// O arduino pega dados de 10 em 10 minutos
-// 1 hora tem 60 minutos 
-// Logo tem 6 regitros por hora 60/10 = 6 correto?
-// Então em um dia é pego 24 x 6 = 144  que é o limit da função buscarDadosHoje.
-// Agora para pegar dados da semana, a gente só multiplica esses 144 por 7, dando 1008
-// Essa foi minha gambiarra 
